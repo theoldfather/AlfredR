5fcafe8a8e36af37343763b308806c2a0efb3bfa
Jeremy Oldfather <theoldfather@gmail.com>
2017-04-13 21:19:22 -0400

    working example

--- examples.R
+++ examples.R
@@ -3,18 +3,12 @@ library(tis)
 library(dplyr)
 .jinit()
 .jaddClassPath(paste0(Sys.getenv("HOME"),"/Projects/TSDiff/out/artifacts/TSDiff_jar/TSDiff.jar"))
-builder<-.jnew("com/oldfather/alfred/VintageListBuilder","GDPC1")
-vlist<-.jcall(builder,"Lcom/oldfather/tsdiff/CompressedAlignedVintageList;","getVintageList")
-vhead<-.jcall(vlist,"Lcom/oldfather/tsdiff/CompressedAlignedVintageNode;","getHead")
-vlist$getFreq()
 
-date_str<-"2014-07-28"
-jdate<-vlist$getVintageStartDate(date_str)
-start_date<-jdate$toString()
-freq<-tolower(vlist$getFreq())
-series<-vlist$getVintageSeries(date_str)
-tis(series,ti(as.Date(start_date),tif(freq)))
 
+get_vlist<-function(series_name){
+  builder<-.jnew("com/oldfather/alfred/VintageListBuilder",series_name)
+  vlist<-.jcall(builder,"Lcom/oldfather/tsdiff/CompressedAlignedVintageList;","getVintageList")
+}
 
 make_tis<-function(vlist,date_str){
   jdate<-vlist$getVintageStartDate(date_str)
@@ -23,6 +17,8 @@ make_tis<-function(vlist,date_str){
   series<-vlist$getVintageSeries(date_str)
   tis(series,ti(as.Date(start_date),tif(freq)))
 }
-make_tis(vlist,"2012-12-31" )%>% tisPlot()
+gdpc1<-get_vlist("GDPC1")
+gdpc1 %>% make_tis("2017-04-13" ) %>% tisPlot()
+gdpc1 %>% make_tis("2010-06-10" ) %>% tisPlot()
 
          
